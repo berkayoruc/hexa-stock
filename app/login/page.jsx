81e158d9108ee11ffee25a0c28779062852a07a6
berkayoruc <berkayoruc@hotmail.com>
2024-07-31 15:43:56 +0300

    login formdata bug fix

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -15,6 +15,7 @@ export default function LoginPage() {
           <input
             type="email"
             id="email"
+            name="email"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="name@flowbite.com"
             required
@@ -30,6 +31,7 @@ export default function LoginPage() {
           <input
             type="password"
             id="password"
+            name="password"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
           />
