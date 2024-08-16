f144b8f81c2985aebbc006c913df9fa35b32074f
berkayoruc <berkayoruc@hotmail.com>
2024-08-16 05:24:02 +0300

    update: menu modal and logout

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -3,12 +3,13 @@
 import { useEffect, useState } from "react";
 import ProductButton from "../components/buttons/ProductButton";
 // import { logout } from "./actions";
-import { getProductsSSR } from "./actions";
+import { getProductsSSR, logout } from "./actions";
 import Link from "next/link";
 
 const ProductsPage = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [menuModal, setMenuModal] = useState(false);
 
   const getProducts = async () => {
     const ssrProducts = await getProductsSSR();
@@ -21,24 +22,62 @@ const ProductsPage = () => {
   }, []);
 
   return (
-    <div className="flex flex-col h-screen">
-      <header className="w-full bg-slate-400 h-16 flex justify-between items-center px-4">
-        <button className="h-12 w-12 p-1 rounded-lg bg-cyan-300"></button>
-        <input className="w-1/2 h-12 bg-slate-300 rounded-lg" type="text" />
-        {/* <form action={logout}>
-          <button className="h-12 w-12 p-1 rounded-lg bg-cyan-300"></button>
-        </form> */}
-        <Link href="/new-product">
-          <button className="h-12 w-12 p-1 rounded-lg bg-cyan-300"></button>
-        </Link>
-      </header>
-      <main className="w-full bg-slate-300 h-screen-4rem overflow-y-scroll grid grid-cols-2 sm:grid-cols-3 p-2 gap-2">
-        {loading && <p>{"Yükleniyor..."}</p>}
-        {products.map((product) => (
-          <ProductButton key={product.id} product={product} />
-        ))}
-      </main>
-    </div>
+    <>
+      <div className="flex flex-col h-screen">
+        <header className="w-full bg-slate-400 h-16 flex justify-between items-center px-4">
+          <button
+            onClick={() => setMenuModal(true)}
+            className="h-12 w-12 p-1 rounded-lg bg-white text-sm font-medium flex items-center justify-center text-center border border-gray-400 text-gray-900"
+          >
+            Menü
+          </button>
+          <input className="w-1/2 h-12 bg-slate-300 rounded-lg pl-2" type="text" placeholder="Ürün ara" />
+          <Link href="/new-product">
+            <button className="h-12 w-12 p-1 rounded-lg bg-white text-sm font-medium flex items-center justify-center text-center border border-gray-400 text-gray-900">
+              Ekle
+            </button>
+          </Link>
+        </header>
+        <main className="w-full bg-slate-300 h-screen-4rem overflow-y-scroll grid grid-cols-2 sm:grid-cols-3 p-2 gap-2">
+          {loading && <p>{"Yükleniyor..."}</p>}
+          {products.map((product) => (
+            <ProductButton key={product.id} product={product} />
+          ))}
+        </main>
+      </div>
+      {menuModal && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
+          <div className="bg-white w-3/4 h-1/2 rounded-lg relative flex flex-col gap-3">
+            <button
+              onClick={() => setMenuModal(false)}
+              className="absolute top-2 right-2"
+            >
+              X
+            </button>
+            <button
+              onClick={() => setMenuModal(false)}
+              className="mt-12 mx-4 rounded py-2 border border-gray-400 bg-white "
+            >
+              Kategori ekle
+            </button>
+            <button
+              onClick={() => setMenuModal(false)}
+              className="mx-4 rounded py-2 border border-gray-400 bg-white "
+            >
+              Kategoriler
+            </button>
+            <form action={logout}>
+              <button
+                style={{ width: "calc(100% - 2rem)" }}
+                className="mx-4 rounded py-2 border border-gray-400 bg-white "
+              >
+                Çıkış
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
