efcd6eaaeb1ada6c8fa450d7f87e3f4595a26484
berkayoruc <berkayoruc@hotmail.com>
2025-04-03 23:47:50 +0300

    product details

--- app/product/page.jsx
+++ app/product/page.jsx
@@ -34,12 +34,30 @@ const ProductPage = ({}) => {
       <header className="w-full bg-slate-400 h-16 flex justify-between items-center px-4">
         <BackButton />
       </header>
-      <main className="w-full bg-slate-300 h-svh-4rem overflow-y-scroll">
-        <div className="flex flex-col items-center justify-center">
-          <div className="h-48 w-48 bg-amber-200 rounded-md"></div>
-          <h1>{product?.name}</h1>
-          <h2>{product?.price}</h2>
-          {url && <QRCode value={url} size={300} />}
+      <main className="w-full bg-slate-300 h-svh-4rem overflow-y-scroll p-2">
+        <div className="flex w-full justify-between items-start gap-4">
+          {url ? (
+            <QRCode value={url} size={300} />
+          ) : (
+            <div className="h-80 w-80 bg-amber-200 rounded-md"></div>
+          )}
+          <div
+            className="flex flex-col items-start justify-center mt-2 gap-2"
+            style={{ width: "calc(100% - 320px)" }}
+          >
+            <h1 className="font-bold text-3xl">{product?.name}</h1>
+            <span className="font-medium text-xl">{`Maliyet: ${
+              product?.purchase_price ?? "-"
+            }₺`}</span>
+            <span className="font-medium text-xl">{`Stok: ${
+              product?.count ?? "-"
+            }`}</span>
+            {product?.category?.name && (
+              <span className="font-medium text-lg italic font-sans text-gray-500">
+                {product.category.name}
+              </span>
+            )}
+          </div>
         </div>
       </main>
     </div>
